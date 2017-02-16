c123b2cf9edf89c2161e0f0ce78906df7d946444
sylvia villegas <sylvia@svillegas.com>
2017-02-15 20:44:13 -0500

    Add active player indicator

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
   $('.box').on('click', function() {
-    $(this).append('<span>O</span>')
+    $(this).append('<span>O</span>');
+    $('.player-x').toggleClass('goes');
+    $('.player-o').toggleClass('goes');
   });
+
+
 });
