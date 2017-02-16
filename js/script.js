8abf53c4fd01c4bd21f87c57169d7fbc1bb6cab9
sylvia villegas <sylvia@svillegas.com>
2017-02-15 22:10:56 -0500

    Mark with x or o depending on active player

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
-
   $('.box').on('click', function() {
+
     if ( $(this).hasClass('marked') === false ) {
       $(this).addClass('marked');
       $('.player-x').toggleClass('goes');
       $('.player-o').toggleClass('goes');
+
+      if ( $('span.player-x').hasClass('goes') === true ) {
+        $(this).text('O');
+      } else {
+        $(this).text('X');
+      }
+
     }
+
   });
+
 
 });
