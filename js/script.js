2aa16d9418f75a8885674aba1c258dd1dcf28c26
sylvia villegas <sylvia@svillegas.com>
2017-07-14 11:05:12 -0400

    Start game winning/ending
    Original attempt at game winning/completion (incomplete)

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
 $(document).ready(function() {
 
   var blur = 0;
+  var wins = ['.r1', '.r2', '.r3', '.c1', '.c2', '.c3', '.d1', '.d2']
+
+  function checkIfWinner(player) {
+    wins.forEach( function(win) {
+
+      if ( $.each(win, function() {$(win).hasClass(player)} ))
+     {console.log(win);};
+   })
+    winGame();
+  };
+
+  function winGame() {
+    $('.winner').text('You win!');
+  };
 
   $('.box').on('click', function() {
-
     if ( $(this).hasClass('marked') === false ) {
       $(this).addClass('marked');
-      $('.player-x').toggleClass('goes');
-      $('.player-o').toggleClass('goes');
-      $('.bg').css('filter', 'blur(' + blur.toString() + 'px)'); // not working
+      $('#player-x').toggleClass('goes');
+      $('#player-o').toggleClass('goes');
+      $('.bg').css('filter', 'blur(' + blur.toString() + 'px)');
       blur++;
 
-      if ( $('span.player-x').hasClass('goes') === true ) {
+      if ( $('#player-x').hasClass('goes')) {
         $(this).append('<i class="fa fa-glass"></i>');
+        $(this).addClass('player-o');
+        checkIfWinner('player-o');
       } else {
         $(this).append('<i class="fa fa-beer"></i>');
+        $(this).addClass('player-x');
+        checkIfWinner('player-x');
       }
 
     }
-
   });
+
+
 
   $('.reload').on('click', function () {
     location.reload(true);
